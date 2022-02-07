8fbdea960718b62505fca0a49834288a9aa12de0
fabian4 <fabian4@163.com>
2022-02-07 17:07:50 +0800

    update

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(ListNode.getFromArrayString("[1,2,3,4,5,6,7]").toString());
+    }
+}
+
+class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public static ListNode getFromArrayString(String list) {
+        String[] split = list.replace("[", "").replace("]", "").split(",");
+        if (split.length == 0) {
+            return null;
+        }
+        ListNode head = new ListNode(Integer.parseInt(split[0]));
+        ListNode tail = head;
+        for (int i = 1; i < split.length; i++) {
+            tail.next = new ListNode(Integer.parseInt(split[i]));
+            tail = tail.next;
+        }
+        return head;
+    }
+
+    @Override
+    public String toString() {
+        ArrayList<Integer> list = new ArrayList<>();
+        while (next!=null){
+            list.add(val);
+            next = next.next;
+        }
+        return list.toString();
+    }
+
+}
