6ae1e50ceb5769996de886a44707272b7cf47c29
fabian4 <fabian4@163.com>
2022-02-25 13:57:04 +0800

    update

--- src/Main.java
+++ src/Main.java
@@ -2,33 +2,30 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(MergeSort(new int[]{5, 135, 2345, 4356, 56, 456, 1, 34325, 34, 234, 235, 435})));
+        int[] arr = new int[]{5, 135, 2345, 4356, 56, 456, 1, 34325, 34, 234, 235, 435};
+        QuickSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
     }
 
-    public static int[] MergeSort(int[] array) {
-        if (array.length < 2) {
-            return array;
+    public static void QuickSort(int[] array, int low, int hight) {
+        if (low < 0 || hight >= array.length || low > hight) return;
+        if (low < hight) {
+            int privotpos = partition(array, low, hight);
+            QuickSort(array, low, privotpos - 1);
+            QuickSort(array, privotpos + 1, hight);
         }
-        int mid = array.length / 2;
-        int[] left = Arrays.copyOfRange(array, 0, mid);
-        int[] right = Arrays.copyOfRange(array, mid, array.length);
-        return merge(MergeSort(left), MergeSort(right));
+
     }
 
-    public static int[] merge(int[] left, int[] right) {
-        int[] result = new int[left.length + right.length];
-        for (int index = 0, i = 0, j = 0; index < result.length; index++) {
-            if (i >= left.length) {
-                result[index] = right[j++];
-            } else if (j >= right.length) {
-                result[index] = left[i++];
-            } else if (left[i] > right[j]) {
-                result[index] = right[j++];
-            } else {
-                result[index] = left[i++];
-            }
+    public static int partition(int[] array, int low, int hight) {
+        int privot = array[low];
+        while (low < hight) {
+            while (low < hight && array[hight] >= privot) --hight;
+            array[low] = array[hight];
+            while (low < hight && array[low] <= privot) ++low;
+            array[hight] = array[low];
         }
-        return result;
-
+        array[low] = privot;
+        return low;
     }
 }
