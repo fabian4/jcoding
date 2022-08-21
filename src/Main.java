9a842c4869637cf144195c97957ad50b0d0a883a
fabian4 <fabian4@163.com>
2022-08-21 10:21:13 +0800

    update

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+        A x = new B();
+        B y = new B();
+        A z = new B();
+
+        x.f(1);
+        ((A)y).f(1);
+        z.f(1);
+    }
+}
+
+class A {
+    void f(int i) {
+        System.out.println(i);
+    }
+}
+
+class B extends A {
+    void f(int i) {
+        System.out.println(2 * i);
     }
 }
